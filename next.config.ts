3bd37ab3aaf8f701442e3cbdae01c545a9114d20
João Victor Franklin <joao.victor@techiasolutions.com.br>
2026-05-03 22:49:18 -0300

    fix: remove incompatible devIndicators to fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false, // Hides the 'N' (Next.js static route) indicator
-  },
+  /* config options here */
 };
 
 export default nextConfig;
